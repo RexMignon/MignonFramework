ecda180359a3cf8f0e747618f718e3758928be3f
Mignon <1342449523@qq.com>
2025-08-11 01:23:00 +0800

    完善setup

--- setup.py
+++ setup.py
@@ -4,16 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    # 库的名称，这将是 pip install 时使用的名字
     name="MignonFramework",
 
-    # 版本号
+
     version="0.1.0",
 
-    # 作者名字
+
     author="Mignon Rex",
 
-    # 作者邮箱
     author_email="rexdpbm@gmail.com",
 
     description="一个为爬虫和后端开发设计的强大Python工具集",
@@ -21,10 +19,10 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
 
-    url="https://github.com/your_username/MignonFramework",
+    url="https://github.com/RexMignon/MignonFramework",
     packages=setuptools.find_packages(),
 
-    # 定义项目依赖的第三方库
+
     install_requires=[
         "requests",
     ],
@@ -34,7 +32,7 @@ setuptools.setup(
 
         "License :: OSI Approved :: MIT License",
 
-        # 目标操作系统
+
         "Operating System :: OS Independent",
 
         "Programming Language :: Python :: 3",
