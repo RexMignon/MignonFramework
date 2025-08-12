221ff6509000b6212f263f86892c5104a3aeb7ed
Mignon <1342449523@qq.com>
2025-08-12 15:54:46 +0800

    REQ

--- MignonFramework/__init__.py
+++ MignonFramework/__init__.py
@@ -9,6 +9,7 @@ PortForwarding 端口转发
 Curl2Request Curl转Request
 MysqlManager: 一个健壮的 MySQL 数据库管理器。
 GenericFileProcessor: 一个高度通用的文件到数据库ETL（提取、转换、加载）工具。
+ProcessFile: 提供一个可配置、可断点续传、高性能的通用文件处理引擎。(默认整文件为json) 它的任务是持续不断地监控一个目录，将文件进行解析、转换，并追加写入到大型结果文件中。
 """
 
 
